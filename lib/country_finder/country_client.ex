67cbe19f452ee9ad8dc01eacb6960f8885512f56
sangeetha <sangeethailango21@gmail.com>
2023-06-20 13:57:15 +0530

    Fix form

--- lib/country_finder/country_client.ex
+++ lib/country_finder/country_client.ex
@@ -1,8 +1,6 @@
 defmodule CountryFinder.CountryClient do
   @behaviour CountryFinder.CountryClientBehaviour
 
-
-
   def all_countries() do
     url = "https://restcountries.com/v3.1/all?fields=flags,name,population,region,capital"
     Finch.build(:get, url) |> Finch.request(CountryFinder.Finch)
